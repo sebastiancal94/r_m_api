17cb292d25d5ea2fc2aa8f2ae37ef3b3de428288
Juan Sebastian Calvo Galvis <ases741@gmail.com>
2023-01-03 10:18:03 -0500

    save 1/3/2012 10:17am

--- src/deploy.sh
+++ src/deploy.sh
@@ -24,6 +24,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:sebastiancal94/rick_and_morty_api.git main:gh-pages
+git push -f git@github.com:sebastiancal94/r_m_api.git main:gh-pages
 
 cd -
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"https://sebastiancal94.github.io/rick_and_morty_api/",
+  base:"https://sebastiancal94.github.io/r_m_api/",
   plugins: [react()],
 })
