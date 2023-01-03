01f823e0c3b555f4091f0f3564cd54faf55e10ee
Juan Sebastian Calvo Galvis <ases741@gmail.com>
2023-01-03 10:01:48 -0500

    save 1/3/2012 10:02am

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"https://sebastiancal94.github.io/Rick_and_Morty_API",
+  base:"https://sebastiancal94.github.io/Rick_and_Morty_API/",
   plugins: [react()],
 })
